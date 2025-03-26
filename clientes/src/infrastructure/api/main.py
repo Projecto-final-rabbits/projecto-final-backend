aa2a5a2a0c9dce3ef6ac114cc000627af9255d8d
Cristhian <ccdelgadop@gmail.com>
2025-03-26 08:42:25 -0500

    test deploy

--- clientes/src/infrastructure/api/main.py
+++ clientes/src/infrastructure/api/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def healthcheck():
     return JSONResponse(content={"message": "OK"}, status_code=200)
 
-@app.get("/test1")
+@app.get("/test2")
 def healthcheck():
     return JSONResponse(content={"message": "OK"}, status_code=200)
 
