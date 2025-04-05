66e58f872b4dd0f45edd2601ccbae6828a2090de
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2025-04-05 16:02:14 -0500

    Se implementa endpoint de creación de productos masivo con coverage de 89% 3

--- bodegas/tests/conftest.py
+++ bodegas/tests/conftest.py
@@ -2,6 +2,8 @@ import sys
 import pytest
 from unittest.mock import MagicMock
 from fastapi.testclient import TestClient
+from unittest.mock import patch
+import httpx
 
 # ✅ Mockear el módulo pubsub.py completo ANTES de que cualquier import lo ejecute
 sys.modules["src.infrastructure.messaging.pubsub"] = MagicMock()
@@ -28,3 +30,9 @@ def client():
     app.dependency_overrides[productos_routes.get_db] = override_get_db
     
     return TestClient(app)
+
+@pytest.fixture(autouse=True)
+def mock_proveedor_existe():
+    with patch("src.application.services.proveedores_service.httpx.get") as mock_get:
+        mock_get.return_value = httpx.Response(200)
+        yield
