92ef800cc1addf50e815db6fdf352bf07bf063d6
Juan Copete <jf.copete@uniandes.edu.co>
2025-04-06 17:30:44 -0300

    clientes_base_complete

--- clientes/src/api/main.py
+++ clientes/src/api/main.py
@@ -4,6 +4,7 @@ from src.api.routes.contacto_cliente_route import router as contactos_router
 from src.api.routes.direccion_entrega_route import router as direcciones_router
 from src.api.routes.tienda_route import router as tiendas_router
 from src.api.routes.pedido_route import router as pedidos_router
+from src.api.routes.health import router as health
 
 from src.config.database import Base, engine
 
@@ -15,6 +16,7 @@ app.include_router(contactos_router)
 app.include_router(direcciones_router)
 app.include_router(tiendas_router)
 app.include_router(pedidos_router)
+app.include_router(health)
 
 if __name__ == "__main__":
     import uvicorn
