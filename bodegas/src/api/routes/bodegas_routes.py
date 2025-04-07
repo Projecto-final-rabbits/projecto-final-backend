db4cfcd8d662828e383bdacfd6af3424f4ec799f
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2025-04-06 21:31:39 -0500

    Prepare to sonarqube

--- bodegas/src/api/routes/bodegas_routes.py
+++ bodegas/src/api/routes/bodegas_routes.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
+
 @router.get("/health")
 def healthcheck():
     return JSONResponse(content={"message": "OK"}, status_code=200)
