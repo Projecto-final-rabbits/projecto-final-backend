5fcadd3a1f613c7b3f15813d07a87c644ade7146
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2025-04-12 08:17:08 -0500

    Change to deploy new  database string

--- bodegas/src/api/routes/bodegas_routes.py
+++ bodegas/src/api/routes/bodegas_routes.py
@@ -3,7 +3,6 @@ from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
-
 @router.get("/health")
 def healthcheck():
     return JSONResponse(content={"message": "OK Sustentacion activa"}, status_code=200)
