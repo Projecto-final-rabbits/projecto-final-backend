613f3e8f17ff0160e135d92c4287aa7e14b57645
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2025-04-26 19:18:13 -0500

    Update pubsub subscription config for bodegas

--- bodegas/src/api/main.py
+++ bodegas/src/api/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.infrastructure.adapters.in_events.pubsub_ventas_event_productos_subscriber import VentasProductosHandler
-from src.infrastructure.messaging.pubsub_ventas_productos_subscriber import PubSubVentasProductosSubscriber
 from src.api.routes.bodegas_routes import router as bodegas_router
 from src.api.routes.productos_routes import router as productos_router
 from src.api.routes.movimientos_routes import router as movimientos_router
 from src.api.routes.inventarios_routes import router as inventario_router
 
 from src.config.database import Base, engine
+
+import os
 
 app = FastAPI()
 
@@ -18,28 +18,24 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+if os.getenv("TESTING") != "true":
+    from src.infrastructure.messaging.pubsub import subscribe_to_topic
+    from src.infrastructure.adapters.in_events.pubsub_ventas_event_productos_subscriber import ventas_productos_handler
+    print("🚀 Iniciando suscripción Pedido bodega sub a Pub/Sub desde Bodegas")
+    subscribe_to_topic(callback=ventas_productos_handler)
+
 app.include_router(bodegas_router)
 app.include_router(productos_router)
 app.include_router(movimientos_router)
 app.include_router(inventario_router)
 
-def _start_pubsub_listener() -> None:
-    handler = VentasProductosHandler()
-    subscriber = PubSubVentasProductosSubscriber(callback=handler)
-    # Arrancamos en daemon para que no bloquee el servidor HTTP
-    subscriber.start(daemon=True)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8003, reload=True)
-    subscriber = PubSubVentasProductosSubscriber(callback=VentasProductosHandler())
-    subscriber.start(daemon=False)
 
 @app.on_event("startup")
 def on_startup():
     print("🔧 Inicializando base de datos...")
     Base.metadata.create_all(bind=engine)
     print("✅ Tablas listas.")
-        # 2. Arrancar Pub/Sub listener
-    _start_pubsub_listener()
-    print("🚀 Pub/Sub listener iniciado.")
--- bodegas/src/infrastructure/adapters/in_events/pubsub_ventas_event_productos_subscriber.py
+++ bodegas/src/infrastructure/adapters/in_events/pubsub_ventas_event_productos_subscriber.py
@@ -3,37 +3,48 @@ import logging
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from typing import Dict, List
+import json
 from src.config.database import engine
 from src.infrastructure.adapters.inventario_repository_sqlalchemy import InventarioRepository
 
-class VentasProductosHandler:
-    def __call__(self, payload: Dict) -> None:
-        productos: List[dict] = payload.get("productos", [])
-        logging.info(f"🟢 Recibido pedido con {len(productos)} ítems")
+# class VentasProductosHandler:
+#     def __call__(self, payload: Dict) -> None:
+#         productos: List[dict] = payload.get("productos", [])
+#         logging.info(f"🟢 Recibido pedido con {len(productos)} ítems")
 
-        with Session(engine) as db:
-            invent_repo = InventarioRepository(db)
+#         with Session(engine) as db:
+#             invent_repo = InventarioRepository(db)
 
-            for p in productos:
-                pid = p.get("producto_id")
-                try:
-                    cantidad = int(p.get("cantidad", 0))
-                except (TypeError, ValueError):
-                    raise HTTPException(
-                        status_code=422,
-                        detail=f"Cantidad inválida para producto {pid}"
-                    )
+#             for p in productos:
+#                 pid = p.get("producto_id")
+#                 try:
+#                     cantidad = int(p.get("cantidad", 0))
+#                 except (TypeError, ValueError):
+#                     raise HTTPException(
+#                         status_code=422,
+#                         detail=f"Cantidad inválida para producto {pid}"
+#                     )
 
-                try:
-                    invent_repo.descontar_stock(pid, cantidad)
-                except ValueError as err:
-                    msg = str(err)
-                    if msg.startswith("No existe inventario"):
-                        raise HTTPException(status_code=404, detail=msg)
-                    if msg.startswith("Stock insuficiente"):
-                        raise HTTPException(status_code=409, detail=msg)
-                    # genérica
-                    raise HTTPException(status_code=400, detail=msg)
+#                 try:
+#                     invent_repo.descontar_stock(pid, cantidad)
+#                 except ValueError as err:
+#                     msg = str(err)
+#                     if msg.startswith("No existe inventario"):
+#                         raise HTTPException(status_code=404, detail=msg)
+#                     if msg.startswith("Stock insuficiente"):
+#                         raise HTTPException(status_code=409, detail=msg)
+#                     # genérica
+#                     raise HTTPException(status_code=400, detail=msg)
 
-            db.commit()
-            logging.info("🟢 Stock descontado correctamente")
+#             db.commit()
+#             logging.info("🟢 Stock descontado correctamente")
+
+def ventas_productos_handler(message):
+    try:
+        print("📩 Mensaje recibido en COMPRAS:", flush=True)
+        data = json.loads(message.data.decode("utf-8"))
+        print(f"🧾 Data: {data}", flush=True)
+
+    except Exception as e:
+        print(f"🚨 Error al procesar mensaje: {e}", flush=True)
+        message.nack()
